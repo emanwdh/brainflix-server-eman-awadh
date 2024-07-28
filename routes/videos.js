ecfff21aa05bfa9fd85aa1006614dfcc72a8be11
Eman Awadh <emanwdh@gmail.com>
2024-07-28 15:37:33 -0400

    Build: Import fs module, read and write to Json file, update get and post request for reading and writing to json file using newVideo object

--- routes/videos.js
+++ routes/videos.js
@@ -1,36 +1,52 @@
 import express from 'express';
-import videoData from '../data/videos.json' with {type: "json"};
 import { v4 as uuidv4 } from 'uuid';
+import fs from "fs";
+import cors from 'cors';
+
+const FILE_PATH = './data/videos.json';
+
+const readData = () => {
+    const videoData = fs.readFileSync(FILE_PATH);
+    const parsedData = JSON.parse(videoData);
+    return parsedData;
+}
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/images', express.static('public/images'));
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    const videoData = readData();
     const videoArray = videoData.map(video => ({id: video.id, title: video.title, channel: video.channel, image: video.image}));
-    res.send(videoArray);
+    res.status(200).json(videoArray);
 });
 
 router.get("/:id", (req, res) => {
+    const videoData = readData();
     const requestedVideo = videoData.filter(video => video.id === req.params.id);
-    res.send(requestedVideo);
+    res.status(200).json(requestedVideo);
 });
 
 router.post("/post", (req, res) => {
-    const { title, description, author, likes, views, channel, timestamp } = req.body;
+    const { title, description, likes, views, channel, timestamp } = req.body;
     const newVideo = {
         id: uuidv4(), 
         title,
         description,
-        image: "http://localhost:5050/images/default.png",
+        image: 'http://localhost:5050/images/default.png',
         channel, 
         likes,
         views,
         timestamp
     };
+    const videoData = readData();
     videoData.push(newVideo); 
-    res.send(videoData);
+    fs.writeFileSync(FILE_PATH, JSON.stringify(videoData));
+
+    res.status(201);
+
 });
 export default router;
