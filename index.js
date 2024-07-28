56a3b61caa149b013223a5e38875537b8f4a4f47
Eman Awadh <emanwdh@gmail.com>
2024-07-28 15:05:24 -0400

    POST: Update Post Request body to take likes, channel, timestamp and views

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 const {PORT, BACKEND_URL} = process.env;
 import videos from "./routes/videos.js";
 import videoData from './data/videos.json' with {type: "json"};
+import { v4 as uuidv4 } from 'uuid';
 
 
 
@@ -13,12 +14,11 @@ app.use(express.json());
 app.use('/images', express.static('public/images'));
 
 
+app.use("/videos", videos);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
-
-app.use("/videos", videos);
 
 
 
--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import videoData from '../data/videos.json' with {type: "json"};
+import { v4 as uuidv4 } from 'uuid';
 
+const app = express();
+app.use(express.json());
+app.use('/images', express.static('public/images'));
 
 const router = express.Router();
 
@@ -9,4 +13,24 @@ router.get("/", (req, res) => {
     res.send(videoArray);
 });
 
+router.get("/:id", (req, res) => {
+    const requestedVideo = videoData.filter(video => video.id === req.params.id);
+    res.send(requestedVideo);
+});
+
+router.post("/post", (req, res) => {
+    const { title, description, author, likes, views, channel, timestamp } = req.body;
+    const newVideo = {
+        id: uuidv4(), 
+        title,
+        description,
+        image: "http://localhost:5050/images/default.png",
+        channel, 
+        likes,
+        views,
+        timestamp
+    };
+    videoData.push(newVideo); 
+    res.send(videoData);
+});
 export default router;
